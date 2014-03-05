388051a01a8dcc4710c7578f5b80c98b2a34862a
Tres Seaver <tseaver@palladion.com>
2014-03-05 15:31:02 -0500

    Coverage

--- repoze/session/_compat.py
+++ repoze/session/_compat.py
@@ -3,13 +3,14 @@ import sys
 PY3 = sys.version_info[0] >= 3
 PY2 = not PY3
 
-if PY3:
+if PY3: #pragma NO COVER Py3k
 
     import pickle
 
     def get_code(method):
         return method.__code__
-else:
+
+else: #pragma NO COVER Python 2
 
     import cPickle as pickle
 
