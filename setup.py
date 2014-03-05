b49c3dfca0f75e906643e647dcb122d5deddf283
Tres Seaver <tseaver@palladion.com>
2014-03-05 13:44:46 -0500

    Nest into github.

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ docs_extras = ['Sphinx']
 
 here = os.path.abspath(os.path.dirname(__file__))
 try:
-    README = open(os.path.join(here, 'README.txt')).read()
-    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+    README = open(os.path.join(here, 'README.rst')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 except:
     README = CHANGES = ''
 
